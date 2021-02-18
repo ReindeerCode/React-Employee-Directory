f3ce65792589739c87221a28b24e220f71c30f1a
MJRudolph <ReindeerCode@gmail.com>
2021-02-18 15:44:07 -0500

    updates to App.css and App.js files - added code to generate 10 users, filter by gender, and reset to 10 users after filter

--- react-employee-directory/src/App.js
+++ react-employee-directory/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    people: [],
+    search: "",
+  };
+
+  componentDidMount() {
+    const data = fetch("https://randomuser.me/api/?results=10");
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  }
+
+  componentWillUnmount() {
+    console.log("About to unmount");
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleSearchClick = () => {
+    const search = this.state.search;
+    const data = fetch(`https://randomuser.me/api/?gender=${search}`);
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  };
+  handleResetClick = () => {
+    const data = fetch("https://randomuser.me/api/?results=10");
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  };
+
+  render() {
+    return (
+      <div className="body">
+        <div className="App">
+          <h1>Employee Directory</h1>
+          <input
+            type="text"
+            value={this.state.search}
+            onChange={this.handleInputChange}
+          />
+          <button type="button" onClick={this.handleSearchClick}>
+            Search Male or Female
+          </button>
+          <br></br>
+          <button type="button" onClick={this.handleResetClick}>
+            Reset
+          </button>
+          {this.state.people.map((person) => (
+            <h1 key={person.email}>{person.email}</h1>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
